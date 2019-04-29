0f896d9b8c36ca6b5e23c5144b9d57455652b691
NurulHakim <nurulhakim2810@gmail.com>
2019-04-29 12:56:28 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bismillah tubesnya sukses dan lancar");
+        System.out.println("tetew");
     }
     
 }
