1f523f6ed4ac758d3d509c9a9eac8a8a6ac6f7dd
NurulHakim <nurulhakim2810@gmail.com>
2019-05-01 10:18:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import gui.login;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +13,14 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bismillah tubesnya sukses dan lancar");
-        System.out.println("tetew");
+        /*System.out.println("Bismillah tubesnya sukses dan lancar");
+        System.out.println("tetew");*/
+        System.out.println("tetewhghjh"); 
+    
+        login a = new login();
+        a.setVisible(true);
+        
+        
     }
     
 }
