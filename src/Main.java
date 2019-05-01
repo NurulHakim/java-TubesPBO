33a9f15add9d7a2b247287de6864b1599296cda7
NurulHakim <nurulhakim2810@gmail.com>
2019-05-01 10:21:27 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         /*System.out.println("Bismillah tubesnya sukses dan lancar");
         System.out.println("tetew");*/
-        System.out.println("tetewhghjh"); 
+        System.out.println("tetew tetewww"); 
     
         login a = new login();
         a.setVisible(true);
