ea58071014332f370460ea76fa0e8a041134dde9
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2019-05-01 10:45:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         /*System.out.println("Bismillah tubesnya sukses dan lancar");
         System.out.println("tetew");*/
-        System.out.println("sumber air su dehjgkkat"); 
+        System.out.println("sumber air su dehjbkdlkvdjgkkat"); 
     
         login a = new login();
         a.setVisible(true);
