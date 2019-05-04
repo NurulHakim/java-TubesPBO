fce383b615f3fbb9ca187bafd9b2d93797563741
VirdannisaSelqy <48021496+VirdannisaSelqy@users.noreply.github.com>
2019-05-05 00:17:58 +0700

    Create formlogin.java

---
+++ src/gui/formlogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+
+/**
+ *
+ * @author Virda
+ */
+public class formlogin {
+    public static void main(String[] args) {
+        formlogin X = new formlogin();
+        X.setVisible(true);
+    }
+
+    private void setVisible(boolean b) {
+        
+    }
+    
+    
+}
