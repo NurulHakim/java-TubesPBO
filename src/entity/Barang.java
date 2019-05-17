64a10c6ae1c19fd28e8f4d0ebd240e094d319ea8
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2019-05-17 10:02:41 +0700

    Update Barang.java

--- src/entity/Barang.java
+++ src/entity/Barang.java
@@ -13,5 +13,48 @@ public class Barang {
     String nama_barang;
     double harga;
     //gambar
+    byte[] gambar;
+    int id_barang;
+    
+    public Barang(int id_barang, String nama_barang, double harga, byte[] gambar){
+      this.id_barang = id_barang;
+      this.nama_barang = nama_barang;
+      this.harga = harga;
+      this.gambar = gambar;
+    }
+
+    
+    public byte[] getGambar(){
+        return gambar;
+    }
+    
+    public void setGambar(byte[] gambar){
+       this.gambar = gambar;
+    }
+    
+    public String getNamabarang(){
+        return nama_barang;
+    }
+    
+    public void setNamabarang(String nama_barang){
+        this.nama_barang = nama_barang;
+    }
+    
+     public double getHarga(){
+        return harga;
+    }
+    
+    public void setHarga(double harga){
+        this.harga = harga;
+    }
+    
+    public int getIdBarang(){
+        return id_barang;
+    }
+    
+    public void setIdBarang(int id_barang){
+        this.id_barang = id_barang;
+    }
+    
     
 }
