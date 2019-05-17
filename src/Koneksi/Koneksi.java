5ddf7ab774b6ec3c5a953d98856ead490cc1fced
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2019-05-17 09:58:50 +0700

    Create Koneksi.java

---
+++ src/Koneksi/Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+
+/**
+ *
+ * @author Virda
+ */
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    Connection con;
+    
+    public Connection getData (){
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost/danusan","root", "");
+        }
+        
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Gagal Terhubung Database!");
+        }
+        
+        return con;
+    }
+}
+
