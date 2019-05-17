c448cd670a1952d264aa0e0eb99bf3b4a8fa373f
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2019-05-17 09:58:59 +0700

    Create BarangDao.java

---
+++ src/Interface/BarangDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import entity.Barang;
+import java.util.List;
+
+
+
+
+/**
+ *
+ * @author ASUS
+ */
+public interface BarangDao {
+    
+    
+    public void save(Barang barang);
+    public void update(Barang barang);
+    public void delete(Barang barang);
+    public List<Barang> getAll();
+    
+    
+}
