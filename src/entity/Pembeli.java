202a2ccd74d2161799f7211dbd18e87530598300
Rahmat Ginting <45379525+ginting24@users.noreply.github.com>
2019-05-17 10:02:37 +0700

    Update Pembeli.java

--- src/entity/Pembeli.java
+++ src/entity/Pembeli.java
@@ -10,10 +10,11 @@ package entity;
  * @author ASUS
  */
 public class Pembeli {
-    char nim;
-    String username, password, nama_pembeli, jk, lokasi, prodi;
-    int angkatan;
-    //gambar
+    int id_pembeli;
+    String username, password, nama_pembeli, jk, lokasi;
+    
+     
+    
     
     public String getUsername(){
         return username;
@@ -31,19 +32,19 @@ public class Pembeli {
         this.password = password;
     }
     
-    public double getNim(){
-        return nim;
+    public int getIdPembeli(){
+        return id_pembeli;
     }
     
-    public void setNim(char nim){
-        this.nim = nim;
+    public void setIdPembeli(int id_pembeli){
+        this.id_pembeli = id_pembeli;
     }
     
-    public String getNama(){
+    public String getNamaPembeli(){
         return nama_pembeli;
     }
     
-    public void setNama(String nama){
+    public void setNamaPembeli(String nama){
         this.nama_pembeli = nama_pembeli;
     }
     
@@ -63,19 +64,7 @@ public class Pembeli {
         this.lokasi = lokasi;
     }
     
-    public String getProdi(){
-        return prodi;
-    }
     
-    public void setProdi(String prodi){
-        this.prodi = prodi;
-    }
     
-    public int getAngkatan(){
-        return angkatan;
-    }
     
-    public void setAngkatan(int angkatan){
-        this.angkatan = angkatan;
-    }
 }
